264b851dcf1b3b51dbd7767b536761b716cf1700
Ramadeep_Yadav <rdeep1208y@gmail.com>
2026-01-14 11:47:44 +0530

    Time: 3 ms (34.84%) | Memory: 13.2 MB (36.66%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -5,41 +5,42 @@ public:
     public:
         DisjointSet(int n){
             parent.resize(n+1);
-            size.resize(n+1, 1);
-            for(int i=0; i<=n; i++){
+            size.assign(n+1, 1);
+
+            for(int i=1; i<=n; i++){
                 parent[i] = i;
             }
         }
-
         int findUpar(int node){
             if(parent[node] == node) return node;
-            return parent[node] = findUpar(parent[node]); // path compression
+            return parent[node] = findUpar(parent[node]);
         }
-
         bool UnionBySize(int u, int v){
-            int pu = findUpar(u);
-            int pv = findUpar(v);
+            int ulp_u = findUpar(u);
+            int ulp_v = findUpar(v);
 
-            if(pu == pv) return false;  // cycle found (redundant edge)
+            if(ulp_u == ulp_v) return false;
 
-            if(size[pu] < size[pv]) swap(pu, pv);
+            if(size[ulp_u] < size[ulp_v]) swap(ulp_u, ulp_v);
 
-            parent[pv] = pu;
-            size[pu] += size[pv];
+            size[ulp_u] += size[ulp_v];
+            parent[ulp_v] = ulp_u;
             return true;
         }
     };
-
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
         DisjointSet ds(n);
 
-        for(auto &e : edges){
-            int u = e[0], v = e[1];
+        vector<int> last;
+        for(auto it : edges){
+            int u = it[0];
+            int v = it[1];
+
             if(!ds.UnionBySize(u, v)){
-                return e;  // first edge that forms a cycle
+                last  = it;
             }
         }
-        return {};
+        return last;
     }
 };
